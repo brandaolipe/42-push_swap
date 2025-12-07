cb1d0dcdf69a6ba7a97145d1ac7fb2c7fc91ab1d
febranda <felipebrandaochaves@gmail.com>
2025-12-07 20:40:22 -0300

    feat(): reverse_split();

---
+++ src/reverse_split.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_split.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 17:02:17 by febranda          #+#    #+#             */
+/*   Updated: 2025/12/07 20:38:13 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+static int	absolute_len(char **arr);
+
+char	*reverse_split(char **arr)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = absolute_len(arr);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (*arr)
+	{
+		while (**arr)
+		{
+			str[i] = **arr;
+			(*arr)++;
+			i++;
+		}
+		arr++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+static int	absolute_len(char **arr)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	j = 0;
+	len = 0;
+	while (arr[j])
+	{
+		i = 0;
+		while (arr[j][i])
+		{
+			len++;
+			i++;
+		}
+		j++;
+	}
+	return (len);
+}
