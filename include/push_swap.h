d4e9f284bb54537ec16218e85555f047212b5254
febranda <felipebrandaochaves@gmail.com>
2025-12-07 21:39:24 -0300

    .

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,30 +6,26 @@
 /*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:14:49 by febranda          #+#    #+#             */
-/*   Updated: 2025/12/03 19:14:38 by febranda         ###   ########.fr       */
+/*   Updated: 2025/12/07 20:30:06 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-
 # define PUSH_SWAP_H
 
 # include <unistd.h>
+# include "libft.h"
 
-typedef struct s_list_a
+void	fill_list_a(char **argv);
+char	*reverse_split(char **arr);
+int		verify_chars(char *numbers);
+
+typedef struct s_list_push
 {
 	int				id;
 	int				number;
-	struct s_list_a	*next;
-	struct s_list_a	*prev;
-}	t_list_a;
-
-typedef struct s_list_b
-{
-	int				id;
-	int				number;
-	struct s_list_b	*next;
-	struct s_list_b	*prev;
-}	t_list_b;
+	struct s_list	*next;
+	struct s_list	*prev;
+}	t_list_push;
 
 #endif
