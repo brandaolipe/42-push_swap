f815b29d090eab70bdeaaa354dcce861794fc3b2
febranda <felipebrandaochaves@gmail.com>
2025-12-14 20:01:21 -0300

    .

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:14:49 by febranda          #+#    #+#             */
-/*   Updated: 2025/12/13 21:27:40 by febranda         ###   ########.fr       */
+/*   Updated: 2025/12/14 19:56:21 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,23 @@
 
 typedef struct s_list_push
 {
-	int				id;
-	int				number;
+	int					id;
+	int					number;
 	struct s_list_push	*next;
 	struct s_list_push	*prev;
 }	t_list_push;
 
 //void	fill_list_a(char **argv);
-char	*reverse_split(char **arr);
-int		verify_arguments(char *args);
-void	add_node(t_list_push **lst, int content);
-int		ft_atoi_enhanced(char *str);
-void	create_list_a(char *args);
+char		*reverse_split(char **arr);
+int			verify_arguments(char *args);
+void		add_node(t_list_push **lst, int content);
+int			ft_atoi_enhanced(char *str);
+t_list_push	*create_list_a(char *args);
+int			check_duplicate_numbers(t_list_push *lst);
+void		process_list_a(char	*args);
+void		free_split(char **arr);
 
 //list functions
-void	add_node(t_list_push **lst, int content);
+void		add_node(t_list_push **lst, int content);
 
 #endif
