ca2402760c354eb79daf34c6962fbd476362ff9b
febranda <felipebrandaochaves@gmail.com>
2025-12-13 21:47:28 -0300

    rename file

--- add_node.c
+++ add_node.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fill_list_a.c                                      :+:      :+:    :+:   */
+/*   add_node.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:49:15 by febranda          #+#    #+#             */
-/*   Updated: 2025/12/11 16:47:36 by febranda         ###   ########.fr       */
+/*   Updated: 2025/12/13 21:45:50 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
-
-t_list_push	fill_list_a(char *args)
-{
-	char		**array_args;
-	t_list_push	head_list_a;
-	int			content;
-
-	head_list_a = NULL;
-	array_args = ft_split(args, ' ');
-	while (*array_args)
-	{
-		content = ft_atoi_enhance(*array_args);
-		add_node(&head_list_a, content);
-		array_args++;
-	}
-	return (head_list_a);
-}
 
 void	add_node(t_list_push **lst, int content)
 {
