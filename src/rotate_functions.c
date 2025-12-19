5d566c0707889692a5727edfb7b49ca6da29bf12
febranda <felipebrandaochaves@gmail.com>
2025-12-19 05:20:19 -0300

    feat(): rotate(), ra(), rb(), rr()

---
+++ src/rotate_functions.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_functions.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 01:04:06 by febranda          #+#    #+#             */
+/*   Updated: 2025/12/19 05:19:01 by febranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+static void	rotate(t_list_push **lst)
+{
+	t_list_push	*first;
+	t_list_push	*temp;
+
+	first = *lst;
+	*lst = first->next;
+	if (*lst)
+		(*lst)->prev = NULL;
+	temp = *lst;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = first;
+	first->prev = temp;
+	first->next = NULL;
+}
+
+void	ra(t_list_push **lst_a)
+{
+	if (!lst_a || !*lst_a || !(*lst_a)->next)
+		return ;
+	rotate(lst_a);
+	ft_putendl_fd("ra", 1);
+}
+
+void	rb(t_list_push **lst_b)
+{
+	if (!lst_b || !*lst_b || !(*lst_b)->next)
+		return ;
+	rotate(lst_b);
+	ft_putendl_fd("rb", 1);
+}
+
+void	rr(t_list_push **lst_a, t_list_push **lst_b)
+{
+	int	rotated;
+
+	rotated = 0;
+	if (lst_a && *lst_a && (*lst_a)->next)
+	{
+		rotate(lst_a);
+		rotated = 1;
+	}
+	if (lst_b && *lst_b && (*lst_b)->next)
+	{
+		rotate(lst_b);
+		rotated = 1;
+	}
+	if (rotated)
+		ft_putendl_fd("rr", 1);
+}
