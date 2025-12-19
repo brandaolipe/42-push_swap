1588f7ff1291835923bc1d8c3d30abb527ce2c86
febranda <felipebrandaochaves@gmail.com>
2025-12-19 05:36:11 -0300

    feat: fill_nodes_id()

--- src/fill_nodes_id.c
+++ src/fill_nodes_id.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fill_nodes_id.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 19:11:35 by febranda          #+#    #+#             */
-/*   Updated: 2025/12/15 21:35:05 by febranda         ###   ########.fr       */
+/*   Updated: 2025/12/19 05:35:05 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,19 @@ void	fill_nodes_id(t_list_push *lst)
 	int			id;
 
 	head = lst;
-	temp_1 = lst;
-	temp_2 = lst;
-	while (temp_1)
+	temp1 = lst;
+	temp2 = lst;
+	while (temp1)
 	{
 		id = 0;
-		while (temp_2)
+		while (temp2)
 		{
-			if (temp_1->number > temp_2->number)
+			if (temp1->number > temp2->number)
 				id++;
-			temp_2 = temp2->next;
+			temp2 = temp2->next;
 		}
-		temp_1->id = id;
-		temp_1 = temp_1->next;
+		temp1->id = id;
+		temp1 = temp1->next;
 		temp2 = head;
 	}
 }
